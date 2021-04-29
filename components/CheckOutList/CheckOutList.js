79e25946941c830ab51e85611696dca7dc50aa37
rickychan0611 <ric0611@gmail.com>
2021-04-29 10:52:51 -0700

    create OrderItme

--- components/CheckOutList/CheckOutList.js
+++ components/CheckOutList/CheckOutList.js
@@ -1,22 +1,13 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
 import styled from 'styled-components';
 import { Sidebar } from 'semantic-ui-react';
-import { useDesktopMediaQuery } from '../Responsive/Responsive';
 import { useRecoilState } from 'recoil';
 import { openCheckOutList as openCheckOutListAtom } from '../../data/atoms.js';
 import { orderItems as orderItemsAtom } from '../../data/orderAtoms.js';
+import OrderItem from '../OrderItem';
 
 const CheckOutList = () => {
-  const router = useRouter();
-  const isDesktop = useDesktopMediaQuery();
   const [openCheckOutList, setOpenCheckOutList] = useRecoilState(openCheckOutListAtom);
-  const [orderItems, setOrderItems] = useRecoilState(orderItemsAtom);
-  const [activeItem, setActiveItem] = useState('');
-
-  const remove = (index) => {
-    setOrderItems(prev => prev.filter((_, i) => i !== index))
-  }
+  const [orderItems, ] = useRecoilState(orderItemsAtom);
 
   const total = () => {
     let counter = 0
@@ -25,20 +16,6 @@ const CheckOutList = () => {
     })
     return counter.toFixed(2)
   }
-
-  const OrderItem = ({ item, index }) => (
-    <ItemContainer>
-      <Qty>
-        <ItemText style={{ minWidth: "30px" }}>{item.qty} x</ItemText>
-        <div>
-          <ItemName>{item.name}</ItemName>
-          {item.option.value !== 0 && <p>• {item.option.option + " " + "+$" + item.option.value}</p>}
-          <Remove onClick={() => remove(index)}>Remove</Remove>
-        </div>
-      </Qty>
-      <ItemText>${item.total.toFixed(2)}</ItemText>
-    </ItemContainer>
-  )
 
   return (
     <SidebarContainer
@@ -91,41 +68,6 @@ const EmptyMsg = styled.div`
   flex-direction: column;
   flex-wrap: nowrap;
   padding: 80px 20px;
-`;
-const ItemContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  justify-content: space-between;
-  padding: 15px 0;
-  border-top: solid 1px #dbdbdb;
-`;
-const Qty = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  justify-content: space-between;
-`;
-const ItemText = styled.p`
-  margin: 0;
-  font-size: 16px;
-`;
-const ItemName = styled.p`
-  margin: 0;
-  font-size: 16px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  display: -webkit-box;
-  -webkit-line-clamp: 1;
-  -webkit-box-orient: vertical;
-  margin: 10px 0 10px 0;
-`;
-const Remove = styled.p`
-  margin: 0;
-  font-size: 12px;
-  font-weight: bold;
-  color: red;
-  cursor: pointer;
 `;
 const OrdersContainer = styled.div`
   display: flex;
---
+++ components/OrderItem/OrderItem.js
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+import { useRecoilState } from 'recoil';
+import { orderItems as orderItemsAtom } from '../../data/orderAtoms.js';
+
+
+const OrderItem = ({ item, index }) => {
+  const [, setOrderItems] = useRecoilState(orderItemsAtom);
+  
+  const remove = (index) => {
+    setOrderItems(prev => prev.filter((_, i) => i !== index))
+  }
+  
+  return (
+  <ItemContainer>
+    <Qty>
+      <ItemText style={{ minWidth: "30px" }}>{item.qty} x</ItemText>
+      <div>
+        <ItemName>{item.name}</ItemName>
+        {item.option.value !== 0 && <p>• {item.option.option + " " + "+$" + item.option.value}</p>}
+        <Remove onClick={() => remove(index)}>Remove</Remove>
+      </div>
+    </Qty>
+    <ItemText>${item.total.toFixed(2)}</ItemText>
+  </ItemContainer>
+)}
+
+const ItemContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  padding: 15px 0;
+  border-top: solid 1px #dbdbdb;
+`;
+const Qty = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+`;
+const ItemText = styled.p`
+  margin: 0;
+  font-size: 16px;
+`;
+const ItemName = styled.p`
+  margin: 0;
+  font-size: 16px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  margin: 10px 0 10px 0;
+`;
+const Remove = styled.p`
+  margin: 0;
+  font-size: 12px;
+  font-weight: bold;
+  color: red;
+  cursor: pointer;
+`;
+
+export default OrderItem;

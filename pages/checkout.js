f777046ac7391f22d27b328e233e9fc5925399d7
rickychan0611 <ric0611@gmail.com>
2021-05-18 13:14:26 -0700

    try to fix checkout

--- pages/checkout.js
+++ pages/checkout.js
@@ -38,125 +38,131 @@ const checkout = () => {
   );
 
   useEffect(() => {
-    appReady && orderDetails && !orderDetails.orderItems[0] && router.push('/');
+    console.log(orderDetails)
+    // appReady && orderDetails && !orderDetails.orderItems[0] && router.push('/');
   }, [orderDetails]);
 
   useEffect(() => {
-    console.log('selections', selections);
-    setShowCheckoutButton(false);
-    return () => setShowCheckoutButton(true);
+    // setShowCheckoutButton(false);
+    // return () => setShowCheckoutButton(true);
   }, []);
 
   return (
     <>
-      <Modal closeIcon open={open} size="tiny" onClose={() => setOpen(false)}>
-        <AddressChange setOpen={setOpen} />
-      </Modal>
-      {orderDetails.store && (
-        <Container>
-          <OrdersContainer>
-            <h4 style={{ margin: 0 }}>Order from</h4>
-            <h2 style={{ margin: 0 }}>{orderDetails.store && orderDetails.store.name}</h2>
-            <Divider />
-            <iframe
-              width="100%"
-              height="250"
-              style={{ border: 10 }}
-              loading="lazy"
-              allowFullScreen
-              src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBugBL6F0x-jyq_4l-6OS1i8Du6yv9bH-s&q=
-              ${orderDetails.store.address}`}></iframe>
-            <Divider />
-
-            <Header>Delivery or Pick-up?</Header>
-            <PickupContainer style={{ justifyContent: 'flex-start' }}>
-              <Row onClick={() => setShippingMethod('Delivery')}>
-                <RadioButton
-                  readOnly
-                  type="radio"
-                  value={'Delivery'}
-                  checked={orderDetails.shippingMethod === 'Delivery'}
-                />
-                <Column>
-                  <H4>Delivery</H4>
-                </Column>
-              </Row>
-              <Row style={{ marginLeft: 40 }} onClick={() => setShippingMethod('Pick-up')}>
-                <RadioButton
-                  readOnly
-                  type="radio"
-                  value={'Pick-up'}
-                  checked={orderDetails.shippingMethod === 'Pick-up'}
-                />
-                <Column>
-                  <H4>Pick-up</H4>
-                </Column>
-              </Row>
-            </PickupContainer>
-
-            {orderDetails.shippingMethod === 'Delivery' && (
-              <>
-                <Header>Your Address</Header>
-                <H4>
-                  <Icon name="point" />
-                  {orderDetails.deliveryAddress.address1},&nbsp;
-                  {orderDetails.deliveryAddress.address2 &&
-                    orderDetails.deliveryAddress.address2 + ','}
-                  &nbsp;
-                  {orderDetails.deliveryAddress.city},&nbsp;
-                  {orderDetails.deliveryAddress.province},&nbsp;
-                  {orderDetails.deliveryAddress.country}
-                  <EditButton />
-                </H4>
-                <H4>
-                  <Icon name="smile outline" />
-                  Instruction: {orderDetails.deliveryAddress.dropoff}
-                  <EditButton />
-                </H4>
-                <H4 style={{ marginLeft: 20 }}>{orderDetails.deliveryAddress.instructions}</H4>
-
-                {/* <Header>Delivery Time</Header>
-                <div>
-                  <Button
-                    color={deliveryTime === 'ASAP' ? 'green' : 'white'}
-                    onClick={() => setDeliveryTime('ASAP')}>
-                    ASAP: 20 - 30min
-                  </Button>
-                  <Button
-                    color={deliveryTime === 'Schedule' ? 'green' : 'white'}
-                    onClick={() => setDeliveryTime('Schedule')}>
-                    Schedule
-                  </Button>
-                </div> */}
-              </>
-            )}
-            <Header>Order Summary</Header>
-            {orderDetails.orderItems &&
-              orderDetails.orderItems[0] &&
-              orderDetails.orderItems.map((item, i) => {
-                return <OrderItem item={item} index={i} key={i} />;
-              })}
-            <Divider />
-            <a onClick={() => router.push('/store/' + orderDetails.orderItems[0].store.slug)}>
-              + Add more items
-            </a>
-            <Divider />
-
-            <TotalAmountList orderDetails={orderDetails} />
-
-            <Divider />
-            <Header>Payment method</Header>
-            <Divider />
-
-            <CheckoutButton onClick={() => {}}>
-              <div>Place Order</div>
-              <div>${orderDetails.total}</div>
-            </CheckoutButton>
-          </OrdersContainer>
-        </Container>
-      )}
+    
     </>
-  );
+  )
+  // return (
+  //   <>
+  //     <Modal closeIcon open={open} size="tiny" onClose={() => setOpen(false)}>
+  //       <AddressChange setOpen={setOpen} />
+  //     </Modal>
+
+  //     {orderDetails.store && (
+  //       <Container>
+  //         <OrdersContainer>
+  //           <h4 style={{ margin: 0 }}>Order from</h4>
+  //           <h2 style={{ margin: 0 }}>{orderDetails.store && orderDetails.store.name}</h2>
+  //           <Divider />
+  //           <iframe
+  //             width="100%"
+  //             height="250"
+  //             style={{ border: 10 }}
+  //             loading="lazy"
+  //             allowFullScreen
+  //             src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBugBL6F0x-jyq_4l-6OS1i8Du6yv9bH-s&q=
+  //             ${orderDetails.store.address}`}></iframe>
+  //           <Divider />
+
+  //           <Header>Delivery or Pick-up?</Header>
+  //           <PickupContainer style={{ justifyContent: 'flex-start' }}>
+  //             <Row onClick={() => setShippingMethod('Delivery')}>
+  //               <RadioButton
+  //                 readOnly
+  //                 type="radio"
+  //                 value={'Delivery'}
+  //                 checked={orderDetails.shippingMethod === 'Delivery'}
+  //               />
+  //               <Column>
+  //                 <H4>Delivery</H4>
+  //               </Column>
+  //             </Row>
+  //             <Row style={{ marginLeft: 40 }} onClick={() => setShippingMethod('Pick-up')}>
+  //               <RadioButton
+  //                 readOnly
+  //                 type="radio"
+  //                 value={'Pick-up'}
+  //                 checked={orderDetails.shippingMethod === 'Pick-up'}
+  //               />
+  //               <Column>
+  //                 <H4>Pick-up</H4>
+  //               </Column>
+  //             </Row>
+  //           </PickupContainer>
+
+  //           {orderDetails.shippingMethod === 'Delivery' && (
+  //             <>
+  //               <Header>Your Address</Header>
+  //               <H4>
+  //                 <Icon name="point" />
+  //                 {orderDetails.deliveryAddress.address1},&nbsp;
+  //                 {orderDetails.deliveryAddress.address2 &&
+  //                   orderDetails.deliveryAddress.address2 + ','}
+  //                 &nbsp;
+  //                 {orderDetails.deliveryAddress.city},&nbsp;
+  //                 {orderDetails.deliveryAddress.province},&nbsp;
+  //                 {orderDetails.deliveryAddress.country}
+  //                 <EditButton />
+  //               </H4>
+  //               <H4>
+  //                 <Icon name="smile outline" />
+  //                 Instruction: {orderDetails.deliveryAddress.dropoff}
+  //                 <EditButton />
+  //               </H4>
+  //               <H4 style={{ marginLeft: 20 }}>{orderDetails.deliveryAddress.instructions}</H4>
+
+  //               {/* <Header>Delivery Time</Header>
+  //               <div>
+  //                 <Button
+  //                   color={deliveryTime === 'ASAP' ? 'green' : 'white'}
+  //                   onClick={() => setDeliveryTime('ASAP')}>
+  //                   ASAP: 20 - 30min
+  //                 </Button>
+  //                 <Button
+  //                   color={deliveryTime === 'Schedule' ? 'green' : 'white'}
+  //                   onClick={() => setDeliveryTime('Schedule')}>
+  //                   Schedule
+  //                 </Button>
+  //               </div> */}
+  //             </>
+  //           )}
+  //           <Header>Order Summary</Header>
+  //           {orderDetails.orderItems &&
+  //             orderDetails.orderItems[0] &&
+  //             orderDetails.orderItems.map((item, i) => {
+  //               return <OrderItem item={item} index={i} key={i} />;
+  //             })}
+  //           <Divider />
+  //           <a onClick={() => router.push('/store/' + orderDetails.orderItems[0].store.slug)}>
+  //             + Add more items
+  //           </a>
+  //           <Divider />
+
+  //           <TotalAmountList orderDetails={orderDetails} />
+
+  //           <Divider />
+  //           <Header>Payment method</Header>
+  //           <Divider />
+
+  //           <CheckoutButton onClick={() => {}}>
+  //             <div>Place Order</div>
+  //             <div>${orderDetails.total}</div>
+  //           </CheckoutButton>
+  //         </OrdersContainer>
+  //       </Container>
+  //     )}
+  //   </>
+  // );
 };
 
 const OrdersContainer = styled.div`

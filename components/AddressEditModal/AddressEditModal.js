d286fcb3a4589157953012e51b2fb5849b63fa5c
rickychan0611 <ric0611@gmail.com>
2021-05-22 21:43:53 -0700

    fix typo

--- components/AddressEditModal/AddressEditModal.js
+++ components/AddressEditModal/AddressEditModal.js
@@ -54,32 +54,27 @@ const AddressEditModal = ({
       <Header content={selectedAddress && selectedAddress.type.toUpperCase()} />
       <Modal.Content>
         <Form onSubmit={saveAddressQuery}>
-          {formItems.map((item, i) => {
-            return (
-              <div key={i}>
-                <Form.Input
-                  fluid
-                  required
-                  label={item.label}
-                  placeholder={item.label}
-                  value={selectedAddress && selectedAddress[item.key]}
-                  onChange={(e) => {
-                    handleChange(e, item.key);
-                  }}
-                  error={err && err[item.key]}
-                />
-              </div>
-            )
-          })}
+          {formItems.map((item, i) => <Form.Input
+            key={i}
+            fluid
+            required
+            label={item.label}
+            placeholder={item.label}
+            value={selectedAddress && selectedAddress[item.key]}
+            onChange={(e) => {
+              handleChange(e, item.key);
+            }}
+            error={err && err[item.key]}
+          />)}
         </Form>
       </Modal.Content>
       <Modal.Actions>
         <Button onClick={() => !loading && setOpenEdit(false)}>
           <Icon name="remove" /> Cancel
           </Button>
-        <Button style={{backgroundColor: "#ff614d", color: "white"}} onClick={() => !loading && saveAddressQuery(false)}>
+        <Button style={{ backgroundColor: "#ff614d", color: "white" }} onClick={() => !loading && saveAddressQuery(false)}>
           {loading ? <><Icon loading name='spinner' />Saving</> : <><Icon name="checkmark" /> Save</>}
-          </Button>
+        </Button>
       </Modal.Actions>
     </Modal>
   );

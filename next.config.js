5a62706338c7146865cd7202825e40149bebea6e
rickychan0611 <ric0611@gmail.com>
2021-06-07 00:09:07 -0700

    enable webpack 5

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
 const nextTranslate = require('next-translate')
 
-// module.exports = nextTranslate()
+module.exports = nextTranslate()
 
-module.exports = {
-  ...nextTranslate(),
+// module.exports = {
+//   ...nextTranslate(),
+//   experimental: {
+//     scrollRestoration: true
+//   }
+// }
+
+module.exports = nextTranslate({
   experimental: {
     scrollRestoration: true
-  }
-}
+  },
+  future: {
+    webpack5: true,
+  },
+})
+
 // module.exports = {
 //   future: {
 //     webpack5: false,

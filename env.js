6124bdf933f2905f987163d66689e27814292a3e
rickychan0611 <ric0611@gmail.com>
2021-07-06 14:28:29 -0700

    gitignore

--- env.js
+++
@@ -1,6 +0,0 @@
-export const HOST_URL = "https://restaurantdem.com"
-export const MAP_API = "AIzaSyBugBL6F0x-jyq_4l-6OS1i8Du6yv9bH-s"
-// export const MAP_API = ""
-
-// Ricky
-// export const MAP_API = "AIzaSyBHJSZlrLMWPcINP1GWunczBAt5bs-ZpzY"
